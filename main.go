019272b34e3f8fb66f78cada860b76828aead2bb
autumnnippert <chrisnippert22@gmail.com>
2025-04-02 13:46:10 -0400

    Added  multi-channel support

--- main.go
+++ main.go
@@ -5,38 +5,87 @@ import (
     "net"
 	"strings"
 	"time"
+	"log"
+	"os"
 	
 	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 )
 
-var server string = "irc.myserver.com"
-var port string = "6667"
-var nick string = "test"
-var user string = "test"
-var realname string = "test"
+var (
+	server string = "irc.autumnnippert.com"
+	port string = "6667"
+	nick string = "card"
+	user string = "card"
+	realname string = "card"
 
-var curr_channel string = ""
-var close bool = false
+	log_file *os.File
+
+	connected_channels = make(map[string]bool)
+	channel_outputs map[string]*tview.TextView
+
+	curr_channel string = ""
+	curr_output *tview.TextView
+	input *tview.InputField
+
+	close bool = false
+
+	app *tview.Application
+)
+
+func mkoutput(title string) *tview.TextView {
+	var output *tview.TextView
+	output = tview.NewTextView().
+		SetDynamicColors(true).
+		SetScrollable(true).
+		SetChangedFunc(func() {
+			app.Draw()
+			output.ScrollToEnd()
+		})
+	// scroll to the bottom
+	// output.SetScrollToEnd(true)
+	output.SetTitle(title).SetBorder(true)
+	return output
+}
 
 func join(conn net.Conn, channel string) {
-	fmt.Fprintf(conn, "JOIN %s\r\n", channel)
+	request := fmt.Sprintf("JOIN %s\r\n", channel)
+	log.Printf("Sending %s\n", (request))
+	conn.Write([]byte(request))
+	log.Printf("Joining %s\n", channel)
+	channel_outputs[channel] = mkoutput(channel)
+	connected_channels[channel] = true
+	curr_channel = channel
+
+	switch_output(channel_outputs[channel])
+	curr_output.Write([]byte(fmt.Sprintf("[green]Joining %s\n", channel)))
 }
-func part(conn net.Conn, channel string, message string) {
-	if message == "" {
-		message = "Bye"
-	}
-	fmt.Fprintf(conn, "PART %s :%s\r\n", channel, message)
+
+func part(conn net.Conn, channel string) {
+	request := fmt.Sprintf("PART %s :leaving\r\n", channel)
+
+	conn.Write([]byte(request))
+	log.Printf("Sending %s\n", request)
+	curr_output.Write([]byte(fmt.Sprintf("[red]Leaving %s", curr_channel)))
+	// remove channel from connected channels and outputs
+	delete(connected_channels, curr_channel)
+	delete(channel_outputs, curr_channel)
+
+	curr_channel = ""
+	switch_output(channel_outputs["app"])
+
+	// focus on input
+	app.SetFocus(input)
 }
-func msg(conn net.Conn, channel string, message string) {
-	fmt.Fprintf(conn, "PRIVMSG %s :%s\r\n", channel, message)
+
+func msg(conn net.Conn, message string) {
+	request := fmt.Sprintf("PRIVMSG %s :%s\r\n", curr_channel, message)
+	log.Printf("Sending %s\n", request)
+	fmt.Fprintf(conn, request)
 }
 
 func get_time() string {
-	// get current time
-	t := time.Now()
-	// format it to 15:04:05
-	return t.Format("02-01-2006 15:04:05")
+	return time.Now().Format("02-01-2006 15:04:05")
 }
 
 func parse_response(line string) string {
@@ -61,71 +110,145 @@ func parse_response(line string) string {
 	return fmt.Sprintf("%19s | %s", get_time(), line)
 }
 
-func parse_input(conn net.Conn, line, curr_channel string, stopApp func()) (string, []string) {
-	var output []string
+func parse_input(conn net.Conn, line, curr_channel string, stopApp func()) {
 	tokens := strings.Fields(line)
 
 	if len(tokens) == 0 {
-		return curr_channel, nil
+		log.Println("Empty input")
+		return
 	}
 
-	switch tokens[0] {
-	case "/quit":
-		conn.Write([]byte("QUIT :bye\r\n"))
-		stopApp()
-		output = append(output, "[yellow]Quitting...")
-	case "/join":
-		if len(tokens) >= 2 {
-			channel := tokens[1]
-			conn.Write([]byte(fmt.Sprintf("JOIN %s\r\n", channel)))
-			output = append(output, fmt.Sprintf("[green]Joining %s", channel))
-			return channel, output
+	if strings.HasPrefix(line, "/") {
+
+		switch tokens[0] {
+		case "/raw":
+			// send raw command to server
+			strings := strings.Join(tokens[1:], " ")
+			conn.Write([]byte(fmt.Sprintf("%s\r\n", strings)))
+			curr_output.Write([]byte(fmt.Sprintf("[white]%19s | [blue]Sending raw command: %s\n", get_time(), strings)))
+			log.Printf("Sending raw command: %s\n", strings)
+		case "/quit":
+			conn.Write([]byte("QUIT :bye\r\n"))
+			stopApp()
+			curr_output.Write([]byte("[red]Quitting...\n"))
+		case "/join":
+			if len(tokens) >= 2 {
+				channel := tokens[1]
+
+				// check if channel is already in use
+				if _, exists := connected_channels[channel]; exists {
+					log.Printf("Already in channel %s\n", channel)
+					curr_output.Write([]byte(fmt.Sprintf("[red]Already in channel %s\n", channel)))
+					return
+				}
+
+				join(conn, channel)
+			}else{
+				curr_output.Write([]byte("[red]Usage: /join #channel\n"))
+			}
+		case "/part":
+			if curr_channel != "" {
+				part(conn, curr_channel)
+			}
+		case "/switch":
+			// switch to another channel or a private message if nick exists
+			if len(tokens) >= 2 {
+				channel := tokens[1]
+				curr_channel = channel
+				switch_output(channel_outputs[channel])
+			} else {
+				curr_output.Write([]byte("[red]Usage: /switch #channel\n"))
+			}
+		case "/channels":
+			// shows the channels you're connected to
+			if len(connected_channels) == 0 {
+				curr_output.Write([]byte("[red]No channels connected\n"))
+			} else {
+				curr_output.Write([]byte("[green]Connected channels:\n"))
+				for _, channel := range connected_channels {
+					curr_output.Write([]byte(fmt.Sprintf("[blue] %s", channel)))
+				}
+			}
 		}
-		output = append(output, "[red]Usage: /join #channel")
-	case "/part":
-		if curr_channel != "" {
-			conn.Write([]byte(fmt.Sprintf("PART %s :leaving\r\n", curr_channel)))
-			output = append(output, fmt.Sprintf("[yellow]Parted %s", curr_channel))
-			return "", output
-		}
-		output = append(output, "[red]You're not in a channel")
-	default:
+	} else {
+
 		if curr_channel == "" {
-			output = append(output, "[red]Join a channel first with /join #channel")
+			log.Printf("No channel selected, cannot send message\n")
+			curr_output.Write([]byte("[red]Join a channel first with /join #channel\n"))
 		} else {
-			conn.Write([]byte(fmt.Sprintf("PRIVMSG %s :%s\r\n", curr_channel, line)))
-			output = append(output, fmt.Sprintf("[white]%19s | [blue]To %s: %s", get_time(), curr_channel, line))
+			request := fmt.Sprintf("PRIVMSG %s :%s\r\n", curr_channel, line)
+			log.Printf("Sending %s\n", request)
+			conn.Write([]byte(request))
+			curr_output.Write([]byte(fmt.Sprintf("[white]%19s | [blue]To %s: %s\n", get_time(), curr_channel, line)))
 		}
 	}
-
-	return curr_channel, output
 }
 
-var output *tview.TextView
+func init_logging(){
+    // Open the log file
+    log_file, err := os.OpenFile("irc.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+    if err != nil {
+        log.Fatal(err)
+    }
 
-func main() {
-	app := tview.NewApplication()
-	app.EnableMouse(true)
+    // Set the log output to the file
+    log.SetOutput(log_file)
+	log.Printf("Starting IRC client\n")
+	log.Printf("Logging to irc.log\n")
+}
 
-	output = tview.NewTextView()
-
-	output.
-		SetDynamicColors(true).
-		SetScrollable(true).
-		SetChangedFunc(func() {
-			app.Draw()
-			output.ScrollToEnd()
-		})
-
-	input := tview.NewInputField().
-		SetLabel("> ").
-		SetFieldWidth(0)
-
+func switch_output(newOutput *tview.TextView) {
 	flex := tview.NewFlex().SetDirection(tview.FlexRow).
-		AddItem(output, 0, 1, false).
+		AddItem(newOutput, 0, 1, false).
 		AddItem(input, 1, 0, true)
 
-	app.SetRoot(flex, true)
+	// create a list of channels
+	channels_list := tview.NewList()
+
+	index := 0
+	for channel := range channel_outputs {
+		channel_index := index
+		channels_list.AddItem(channel, "", 0, func() {
+			// switch to the selected channel
+			switch_output(channel_outputs[channel])
+			curr_channel = channel
+
+			channels_list.SetCurrentItem(channel_index)
+
+			curr_output.Write([]byte(fmt.Sprintf("[green]Switching to %s\n", channel)))
+		})
+		if channel == curr_channel {
+			channels_list.SetCurrentItem(index)
+		}
+		index++
+	}
+	channels_list.SetBorder(true).SetTitle("Channels").SetTitleAlign(tview.AlignLeft)
+
+	root := tview.NewFlex().SetDirection(tview.FlexColumn).
+		AddItem(channels_list, 20, 1, true).
+		AddItem(flex, 0, 1, false)
+
+	app.SetRoot(root, true)
+
+	curr_output = newOutput
+}
+
+func main() {
+    init_logging()
+	defer log_file.Close()
+    
+	app = tview.NewApplication()
+	app.EnableMouse(true)
+
+	curr_output = mkoutput("app")
+	channel_outputs = make(map[string]*tview.TextView)
+	channel_outputs["app"] = curr_output
+
+	input = tview.NewInputField().
+		SetLabel("> ").
+		SetFieldWidth(0)
+		
+	switch_output(curr_output)
 
 	// Connect to IRC
 	server_str := fmt.Sprintf("%s:%s", server, port)
@@ -133,17 +256,41 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-
+    
+	log.Printf("Connecting to %s as %s\n", server_str, nick)
 	fmt.Fprintf(conn, "NICK %s\r\n", nick)
 	fmt.Fprintf(conn, "USER %s 0 * :%s\r\n", user, user, realname)
 
+	fmt.Fprintf(conn, "i don't know why but there is some stuff in my tcp so the first command doesn't sent unless this is here :shrug:\n")
+
+    // check if server said username taken or not
+    buf := make([]byte, 4096)
+    n, err := conn.Read(buf)
+    if err != nil {
+        panic(err)
+    }
+    lines := strings.Split(string(buf[:n]), "\r\n")
+    for _, line := range lines {
+        if strings.Contains(line, "433") {
+            curr_output.Write([]byte("[red]Username taken, please try again\n"))
+            log.Printf("Username taken, please try again\n")
+            app.Stop()
+            return
+        }
+        if strings.Contains(line, "001") {
+            curr_output.Write([]byte("[green]Connected to server\n"))
+            log.Printf("Connected to server %s\n", server_str)
+            break
+        }
+    }
+    
 	// --- Reader goroutine
 	go func() {
 		buf := make([]byte, 4096)
 		for {
 			n, err := conn.Read(buf)
 			if err != nil {
-				output.Write([]byte("[red]Disconnected from server\n"))
+				curr_output.Write([]byte("[red]Disconnected from server\n"))
 				app.Stop()
 				return
 			}
@@ -156,8 +303,9 @@ func main() {
 					// Respond to keepalive
 					pong := "PONG" + line[4:] + "\r\n"
 					conn.Write([]byte(pong))
+					continue
 				}
-				output.Write([]byte(parse_response(line) + "\n"))
+				curr_output.Write([]byte(parse_response(line) + "\n"))
 			}
 		}
 	}()
@@ -166,17 +314,16 @@ func main() {
 	input.SetDoneFunc(func(key tcell.Key) {
 			line := input.GetText()
 			input.SetText("")
+			log.Printf("Input: %s\n", line)
 		
-			newChannel, feedback := parse_input(conn, line, curr_channel, app.Stop)
-			curr_channel = newChannel
-		
-			for _, msg := range feedback {
-				output.Write([]byte(msg + "\n"))
-			}
+			parse_input(conn, line, curr_channel, app.Stop)
 		})
 
 	if err := app.Run(); err != nil {
 		panic(err)
 	}
+
+	// Close the connection
+	conn.Close()
 
 }
